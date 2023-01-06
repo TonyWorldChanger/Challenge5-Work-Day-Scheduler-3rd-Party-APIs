fb5727e74d9b54a63a1ba14f75cf4d1bf486b280
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-01-05 22:41:00 -0600

    adding date and time to page

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+// TODO: Add code to display the current date in the header of the page.
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -19,5 +23,5 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  
 });
