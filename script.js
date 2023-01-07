7a919445e90f9523ee174c9391413d42f33fa0dc
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-01-06 21:58:43 -0600

    save button change

--- script.js
+++ script.js
@@ -25,38 +25,11 @@ $(".saveBtn").on("click", saveSchedule);
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  function getSchedule () {
-    for (var i = 0; i < totalHours; i++) {
-      var realTime = i + 8;
-      var getSchedule = localStorage.getItem(`i-${realTime}`);
-      $(`.${realTime}`).val(getSchedule);
-      console.log("schedule");
-    }
-  }
+ 
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  function timeStamp() {
-    for (var hour = 0; hour < totalHours; hour++) { 
-        var realHour = hour + 8;
-        if (currentHour == realHour) {
-            currentState = 'present';
-        } else if (currentHour < realHour) {
-            currentState = 'past';
-        } else {
-            currentState = 'future';
-        }
-
-        
-
-    }
-
-    getSchedule();
-}
   
-
-  
-
